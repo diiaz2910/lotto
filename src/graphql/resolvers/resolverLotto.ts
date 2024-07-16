c3bd9d79c76b6c1d82a28503863e92385ec36326
Ricardo <diiaz2910@gmail.com>
2024-07-16 18:32:18 +1200

    Schema and Resolvers OK for Lotto. Mutation and Query running Graphql side. Update and Delete pending.

--- src/graphql/resolvers/resolverLotto.ts
+++ src/graphql/resolvers/resolverLotto.ts
@@ -1,10 +1,26 @@
 import { Db } from "mongodb";
 import { ObjectId } from "mongodb";
 import { IResolvers } from "@graphql-tools/utils";
+import { create } from "domain";
 
-const resolversLotto = {
+const resolversLotto: IResolvers = {
   Query: {
     _empty: () => "Hello world",
+  },
+  Mutation: {
+    async createCombination(root: void, args: any, context: { db: Db }) {
+      try {
+        const regexp = new RegExp(args.combination, "i");
+        const exist = await context.db
+          .collection("lotto")
+          .findOne({ name: regexp });
+        if (exist) {
+          return "Combination already exists";
+        }
+        await context.db.collection("lotto").insertOne(args.combination);
+      } catch (error) {}
+      return "Combination created successfully";
+    },
   },
 };
 
