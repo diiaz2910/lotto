989d35c9f5bcbd805513a29b00c790feb86f7eb2
Ricardo <diiaz2910@gmail.com>
2024-07-17 21:06:10 +1200

    Lotto Resolver updated to add query function and collections added to collections.ts

--- src/graphql/resolvers/resolverLotto.ts
+++ src/graphql/resolvers/resolverLotto.ts
@@ -3,21 +3,32 @@ import { ObjectId } from "mongodb";
 import { IResolvers } from "@graphql-tools/utils";
 import { create } from "domain";
 
+// collections
+import { LOTTO_COLLECTION } from "../../mongo/collections";
+
 const resolversLotto: IResolvers = {
   Query: {
-    _empty: () => "Hello world",
+    async getCombinations(root: void, args: any, context: { db: Db }) {
+      try {
+        return await context.db.collection(LOTTO_COLLECTION).find().toArray();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   Mutation: {
     async createCombination(root: void, args: any, context: { db: Db }) {
       try {
-        const regexp = new RegExp(args.combination, "i");
+        const regexp = new RegExp(args.combination.numbers, "i");
         const exist = await context.db
-          .collection("lotto")
-          .findOne({ name: regexp });
+          .collection(LOTTO_COLLECTION)
+          .findOne({ numbers: { $eq: args.combinations.numbers } });
         if (exist) {
           return "Combination already exists";
         }
-        await context.db.collection("lotto").insertOne(args.combination);
+        await context.db
+          .collection(LOTTO_COLLECTION)
+          .insertOne(args.combination);
       } catch (error) {}
       return "Combination created successfully";
     },
---
+++ src/mongo/collections.ts
@@ -0,0 +1,1 @@
+export const LOTTO_COLLECTION = "lotto";
