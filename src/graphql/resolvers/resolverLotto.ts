4631de98e76869e69d4a210dea7fe8f56b9baa3a
Ricardo <diiaz2910@gmail.com>
2024-07-18 19:42:27 +1200

    Added update combination to schema and resolver for lotto

--- src/graphql/resolvers/resolverLotto.ts
+++ src/graphql/resolvers/resolverLotto.ts
@@ -19,7 +19,7 @@ const resolversLotto: IResolvers = {
   Mutation: {
     async createCombination(root: void, args: any, context: { db: Db }) {
       try {
-        // Sort numbers before no duplication is allowed
+        // Sort numbers before duplication is not allowed
         const sortedNumbers = [...args.combination.numbers].sort(
           (a, b) => a - b
         );
@@ -30,12 +30,33 @@ const resolversLotto: IResolvers = {
         if (exist) {
           return "Combination already exists";
         }
-        // Saving the combination in the database with the sorted numbers
+        // Saving the combination in the database with numbers sorted
         await context.db
           .collection(LOTTO_COLLECTION)
           .insertOne({ numbers: sortedNumbers });
       } catch (error) {}
       return "Combination created successfully";
+    },
+    async updateCombinations(root: void, args: any, context: { db: Db }) {
+      try {
+        const { _id, ...fieldsToUpdate } = args.combination;
+        const exist = await context.db
+          .collection(LOTTO_COLLECTION)
+          .findOne({ _id: ObjectId.createFromHexString(_id) });
+        if (exist) {
+          await context.db
+            .collection(LOTTO_COLLECTION)
+            .updateOne(
+              { _id: ObjectId.createFromHexString(_id) },
+              { $set: fieldsToUpdate }
+            );
+          return "Combination updated successfully";
+        } else {
+          return "Combination not found";
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
