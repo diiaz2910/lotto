01d3915bae1cf2bd4d14369fe5603f3755578181
Ricardo <diiaz2910@gmail.com>
2024-07-20 15:02:26 +1200

    PowerBall Query and Mutation added missing validation rules. Bonus and Strike schema and resolver updated

--- src/graphql/resolvers/resolverPowerBall.ts
+++ src/graphql/resolvers/resolverPowerBall.ts
@@ -1,10 +1,36 @@
-import { Db } from "mongodb";
-import { ObjectId } from "mongodb";
+import { Db, ObjectId } from "mongodb";
 import { IResolvers } from "@graphql-tools/utils";
 
-const resolversPowerBall = {
+//collections
+import { POWERBALL_COLLECTION } from "../../mongo/collections";
+
+const resolversPowerBall: IResolvers = {
   Query: {
-    _empty: () => "Hello world",
+    async getPowerBalls(root: void, args: any, context: { db: Db }) {
+      try {
+        return await context.db
+          .collection(POWERBALL_COLLECTION)
+          .find()
+          .toArray();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+  },
+  Mutation: {
+    async createPowerBall(root: void, args: any, context: { db: Db }) {
+      try {
+        const sortedNumbers = [...args.powerBall.numbers].sort((a, b) => a - b);
+        await context.db
+          .collection(POWERBALL_COLLECTION)
+          .insertOne({ numbers: sortedNumbers });
+        return "PowerBall created successfully";
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
   },
 };
 
