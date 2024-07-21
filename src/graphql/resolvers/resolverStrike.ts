238bb56366393c58ed4bba9f73e66fcabe488e56
Ricardo <diiaz2910@gmail.com>
2024-07-21 17:52:58 +1200

    Strike schema and resolver updated

--- src/graphql/resolvers/resolverStrike.ts
+++ src/graphql/resolvers/resolverStrike.ts
@@ -1,10 +1,33 @@
-import { Db } from "mongodb";
-import { ObjectId } from "mongodb";
+import { Db, ObjectId } from "mongodb";
 import { IResolvers } from "@graphql-tools/utils";
 
-const resolversStrike = {
+// collections
+import { STRIKE_COLLECTION } from "../../mongo/collections";
+
+const resolversStrike: IResolvers = {
   Query: {
-    _empty: () => "Hello world",
+    async getStrikes(root: void, args: any, context: { db: Db }) {
+      try {
+        return await context.db.collection(STRIKE_COLLECTION).find().toArray();
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+  },
+  Mutation: {
+    async createStrike(root: void, args: any, context: { db: Db }) {
+      try {
+        const sortedNumbers = [...args.strike.numbers].sort((a, b) => a - b);
+        await context.db
+          .collection(STRIKE_COLLECTION)
+          .insertOne({ numbers: sortedNumbers });
+        return "Strike created successfully";
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
   },
 };
 
