4ad4132e21fa58a3dd00cbbccb9fdc1c2de34ed0
Ricardo <diiaz2910@gmail.com>
2024-07-21 15:16:39 +1200

    Bonus schema and resolver added

--- src/graphql/resolvers/resolversBonus.ts
+++ src/graphql/resolvers/resolversBonus.ts
@@ -1,10 +1,33 @@
-import { Db } from "mongodb";
-import { ObjectId } from "mongodb";
+import { Db, ObjectId } from "mongodb";
 import { IResolvers } from "@graphql-tools/utils";
 
-const resolversBonus = {
+//collections
+import { BONUS_COLLECTION } from "../../mongo/collections";
+
+const resolversBonus: IResolvers = {
   Query: {
-    _empty: () => "Hello world",
+    async getBonuses(root: void, args: any, context: { db: Db }) {
+      try {
+        return await context.db.collection(BONUS_COLLECTION).find().toArray();
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+  },
+  Mutation: {
+    async createBonus(root: void, args: any, context: { db: Db }) {
+      try {
+        const sortedNumbers = [...args.bonus.numbers].sort((a, b) => a - b);
+        await context.db
+          .collection(BONUS_COLLECTION)
+          .insertOne({ numbers: sortedNumbers });
+        return "Bonus created successfully";
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
   },
 };
 
